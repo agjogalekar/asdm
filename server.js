b05596e7d88a7714b2555b3a6d7bc4a807accf4d
Akshay Jogalekar <agjogalekar@gmail.com>
2019-05-29 15:55:11 +0530

    After Modification

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const categoryRouter = require('./category');
 const productRouter = require('./product');
 
-const app = expresss();
+const app = express();
 app.use(function(req,res,next)
 {
     res.header("Accesss-Control-Allow-Origin","*");
