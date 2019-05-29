21ae7845db5ca2c2c8c6fcc3be6e766a31fc5ad1
Akshay Jogalekar <agjogalekar@gmail.com>
2019-05-29 15:40:12 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const categoryRouter = require('./category');
+const productRouter = require('./product');
+
+const app = expresss();
+app.use(function(req,res,next)
+{
+    res.header("Accesss-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Methods","GET,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With,Content-TypeError,Accept");
+    next();
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+app.use(categoryRouter);
+app.use(productRouter);
+
+app.listen(3000,'0.0.0.0',()=>{
+    console.log("server started on port number 3000");
+});
